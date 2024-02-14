c7b70254d28e462e2417c8a1470ea66660f7c034
DiyaBhakuni <151662494+DiyaBhakuni@users.noreply.github.com>
2024-02-14 17:19:12 +0530

    Add files via upload

---
+++ appletcalc.java
@@ -0,0 +1,21 @@
+import java.applet.*; import 
+java.awt.*; 
+public class appletcalc extends Applet{ public 
+void paint(Graphics g){ int i = 
+Integer.parseInt(getParameter("Value1")); int j= 
+Integer.parseInt(getParameter("Value2")); int 
+result=1; 
+int ch= Integer.parseInt(getParameter("Choice")); 
+if(ch==1){ 
+result=i+j; } 
+else if(ch==2){ 
+result=i-j; } 
+else if(ch==3){ 
+result=i*j; } 
+else if(ch==4){ 
+result =i/j; 
+} 
+String result1= Integer.toString(result); 
+g.drawString("The result is "+result1, 100 ,100); 
+} 
+}
